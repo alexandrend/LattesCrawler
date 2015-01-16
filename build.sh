5fc02f2c9b29c6ca1afec04e54ecd72d2a371f00
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2015-01-16 00:48:37 -0300

    build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ rm -r bin
 mkdir bin
 javac -cp .:lib/jsoup-1.7.3.jar -d bin src/br/ufpb/ci/labsna/lattescrawler/*.java
 cd bin
-java -cp .:../lib/jsoup-1.7.3.jar br.ufpb.ci.labsna.lattescrawler.LattesCrawler ../codelattes.txt 1 > ../result.gml
+java -cp .:../lib/jsoup-1.7.3.jar br.ufpb.ci.labsna.lattescrawler.LattesCrawler ../codelattes.txt 2 > ../result.gml
